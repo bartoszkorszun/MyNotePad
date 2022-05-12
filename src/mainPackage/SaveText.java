6e284e467b024d5a2247fd540df5c0a6c71c3f26
bartoszkorszun <bartoszkorszun@gmail.com>
2022-05-12 11:01:58 +0200

    SaveText.java has been added

---
+++ src/mainPackage/SaveText.java
@@ -0,0 +1,16 @@
+package mainPackage;
+
+public class SaveText {
+
+	String saveText;
+	
+	public void setSavedText(String st) {
+		
+		saveText = st;
+	}
+	
+	public String getSavedText() {
+		
+		return saveText;
+	}
+}
